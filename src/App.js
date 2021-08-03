5207b927c928c0f9ca7310c4be9fd7c3847ae476
DoctorTone <drtonesoftware@gmail.com>
2021-08-03 12:08:13 +0100

    Fix background colour.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Sphere from "./components/Sphere";
 function App() {
   return (
     <Canvas camera={{ position: [0, 0, 50] }}>
-      <pointLight position={[30, 30, 0]} />
+      <pointLight position={[100, 100, 100]} />
       <Sphere position={[0, 0, 0]} />
     </Canvas>
   );
