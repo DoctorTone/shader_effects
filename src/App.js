f2ffff1315874e40ae356bd8d91ef259fc5e00f6
DoctorTone <drtonesoftware@gmail.com>
2021-08-04 15:36:53 +0100

    Add light to scene.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { OrbitControls } from "@react-three/drei";
 function App() {
   return (
     <Canvas camera={{ position: [20, 20, 50] }}>
+      <pointLight position={[30, 30, 30]} />
       <Sphere position={[0, 0, 0]} />
       <Box position={[20, 20, 20]} />
       <OrbitControls />
