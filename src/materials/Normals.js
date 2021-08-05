eed3005eeb311ba3f87e83327c585447a6aba32d
DoctorTone <drtonesoftware@gmail.com>
2021-08-05 16:45:16 +0100

    Add normal material shader.

---
+++ src/materials/Normals.js
@@ -0,0 +1,24 @@
+import * as THREE from "three";
+import { extend } from "@react-three/fiber";
+
+export class NormalMaterial extends THREE.ShaderMaterial {
+  constructor() {
+    super({
+      vertexShader: `
+            varying vec2 vUv;
+
+            void main() {
+                vUv = uv;
+                gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+            }`,
+      fragmentShader: `
+                varying vec2 vUv;
+
+                voiod main() {
+                    gl_FragColor = vec4(vUv.x, vUv.y, 1.0, 1.0);
+                }`,
+    });
+  }
+}
+
+extend({ NormalMaterial });
