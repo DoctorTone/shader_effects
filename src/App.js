e523abbaf61d0e298ea5f22c6073aa93b55901a0
DoctorTone <drtonesoftware@gmail.com>
2021-08-05 16:36:04 +0100

    Added sphere to scene.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Cube from "./components/Cube";
 import Box from "./components/Box";
+import Sphere from "./components/Sphere";
 import { OrbitControls } from "@react-three/drei";
 import ControlPanel from "./components/ControlPanel";
 import Title from "./components/Title";
@@ -20,6 +21,7 @@ function App() {
         <pointLight position={[30, 30, 30]} />
         <Suspense fallback={null}>
           {currentShader === "Cube" && <Cube position={[0, 0, 0]} />}
+          {currentShader === "Sphere" && <Sphere position={[0, 0, 0]} />}
         </Suspense>
         <Box position={[20, 20, 20]} />
         <OrbitControls />
--- src/components/Sphere.js
+++ src/components/Sphere.js
@@ -1,7 +1,13 @@
+import { meshBounds } from "@react-three/drei";
 import React from "react";
 
-const Sphere = () => {
-  return <div></div>;
+const Sphere = (props) => {
+  return (
+    <mesh {...props}>
+      <sphereBufferGeometry args={[10, 64, 64]} />
+      <meshBasicMaterial color="blue" />
+    </mesh>
+  );
 };
 
 export default Sphere;
