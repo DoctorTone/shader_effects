32667c2af817f8ae382da750b87b1c65f0ffaac1
DoctorTone <drtonesoftware@gmail.com>
2021-08-05 11:24:26 +0100

    Add bounce status to store.

--- src/state/store.js
+++ src/state/store.js
@@ -3,6 +3,8 @@ import create from "zustand";
 const useStore = create((set) => ({
   textureEnabled: true,
   setTextureEnabled: (status) => set({ textureEnabled: status }),
+  bounceEnabled: true,
+  setBounceEnabled: (status) => set({ bounceEnabled: status }),
 }));
 
 export default useStore;
