ba7a94b0c7e5107d5a97ee8de80ec9eaa94470fa
DoctorTone <drtonesoftware@gmail.com>
2021-08-05 13:58:31 +0100

    Added title component.

--- src/materials/Shiny.js
+++ src/materials/Shiny.js
@@ -35,7 +35,7 @@ export class ShinyMaterial extends THREE.ShaderMaterial {
       fragmentShader: `
             vec3 shading(vec3 N, vec3 V, vec3 L, bool specularEnabled) {
                 // Materials
-                vec3 Ka = vec3(0.1, 0.1, 0.1); // Ambient
+                vec3 Ka = vec3(0.2, 0.2, 0.2); // Ambient
                 vec3 Kd = vec3(0.1, 0.1, 0.1); // Diffuse
                 vec3 Ks = vec3(0.5, 0.5, 0.5); // Specular
                 float shininess = 32.0; // Shininess
