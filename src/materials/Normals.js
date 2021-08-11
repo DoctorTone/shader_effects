1cc066b04c24486233df6050b12d54f433a630e9
DoctorTone <drtonesoftware@gmail.com>
2021-08-11 14:42:40 +0100

    Different z colour.

--- src/materials/Normals.js
+++ src/materials/Normals.js
@@ -15,7 +15,7 @@ export class NormalMaterial extends THREE.ShaderMaterial {
                 varying vec2 vUv;
 
                 void main() {
-                    gl_FragColor = vec4(vUv.x, vUv.y, 1.0, 1.0);
+                    gl_FragColor = vec4(vUv.x, vUv.y, (vUv.x + vUv.y), 1.0);
                 }`,
     });
   }
